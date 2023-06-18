1db2c79ba6e56e4155cdda68a2efae301fa3fbb1
Leah <leahxharden@gmail.com>
2023-06-18 19:42:55 -0400

    merged everything in to one page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Pages/Home/Home';
 import About from './components/Pages/AboutMe/AboutMe';
 import Contact from './components/Pages/Contact/Contact';
-import Resume from './components/Pages/Resume/Resume';
+
 import Portfolio from './components/Pages/Portfolio/Portfolio';
 
 import './App.css'; // Import the CSS file
@@ -17,16 +17,23 @@ import './App.css'; // Import the CSS file
 function App() {
   return (
     <div className='background'>
+      <Home />
+      <About />
+      <Portfolio />
+      <Contact />
 
-      <Router>
+
+
+
+
+      {/* <Router>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/resume" element={<Resume />} />
           <Route path="/portfolio" element={<Portfolio />} />
         </Routes>
-      </Router>
+      </Router> */}
     </div>
   )
 }
