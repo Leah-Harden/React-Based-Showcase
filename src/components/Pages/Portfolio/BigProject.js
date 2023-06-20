370707065b559a139b3907cf33cd252db6ab8eff
Leah <leahxharden@gmail.com>
2023-06-20 16:12:41 -0400

    added shadow and resume

--- src/components/Pages/Portfolio/BigProject.js
+++ src/components/Pages/Portfolio/BigProject.js
@@ -3,11 +3,14 @@ function BigProject() {
         <div>
 
             <div className="bigprojectCard">
-                <a href='https://github.com/Qaizen/curious-chronicles' className="bigprojectA">
-                    <img src="/photos/projects/CC.png" className="bigprojectPhoto" alt='The front page of Curious Chronicles' />
-                    <h2 className="bigprojectTitle cavent">Curious Chronicles
-                    </h2>
-                </a>
+                <div className="shadowBigProject">
+
+                    <a href='https://github.com/Qaizen/curious-chronicles' className="bigprojectA">
+                        <img src="/photos/projects/CC.png" className="bigprojectPhoto" alt='The front page of Curious Chronicles' />
+                        <h2 className="bigprojectTitle cavent">Curious Chronicles
+                        </h2>
+                    </a>
+                </div>
                 <p className="bigprojectDesc crimson">The development of this sophisticated web application is driven by the utilization of an extensive technology stack, including HTML, CSS, JavaScript, MongoDB, Express, React Node, GraphQL, and Node Package Manager. Its purpose is to establish an innovative online platform specifically designed for children's journaling.</p>
             </div>
 
---
+++ src/components/Pages/Resume/ResumeText.js
@@ -0,0 +1,11 @@
+
+
+
+function Resume() {
+    return (
+
+        <h2> Here is my resume <a href="#"></a></h2>
+
+    )
+}
+export default Resume;
