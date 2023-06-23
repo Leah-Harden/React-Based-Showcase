9493c9f25e0a8160b97f3682f14bdd65769c625c
Leah <leahxharden@gmail.com>
2023-06-23 15:56:29 -0400

    added space

--- src/components/Pages/Resume/ResumeText.js
+++ src/components/Pages/Resume/ResumeText.js
@@ -5,7 +5,7 @@ function Resume() {
     return (
         <>
             <div className="emptyspace"></div>
-            <h2 className="resumePart crimson" id="Resume"> Here is my <a href="Leah-Harden-Resume.pdf">-Resume-</a></h2>
+            <h2 className="resumePart crimson" id="Resume"> Here is my&nbsp;<a href="Leah-Harden-Resume.pdf">Resume</a></h2>
             <div className="emptyspace"></div>
         </>
     )
